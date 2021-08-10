ebb941dfd73bf0e2516175c35d4f3e2cddbf45ef
Michael Newman <therealmichaelnewman@gmail.com>
2021-08-10 12:59:45 -0400

    commit tree 9e2cfef6bcfdc0c10f5b1146733648840eff7046

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks (pickup) {
+    return Math.abs(42 - pickup);
+}
+
+function distanceFromHqInFeet (pickup){
+    return distanceFromHqInBlocks(pickup) * 264;
+}
+
+function distanceTravelledInFeet (start, destination){
+   return Math.abs((start - destination) * 264);
+}
+
+
+function calculatesFarePrice(start, destination) {
+
+    const distance = distanceTravelledInFeet (start, destination);
+
+
+    if (distance <= 400) {
+        return 0;
+    } else if (distance > 400 && distance <= 2000) {
+        return Math.abs(((start - destination) * 264) - 400) * .02
+    } else if (distance > 2000 && distance <= 2500) {
+        return 25;
+    } else {
+        return "cannot travel that far";
+    }
+}
